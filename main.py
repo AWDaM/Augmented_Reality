88b03024a0b133ff6cf02121a33ef4e7839d34fd
Lorién <lorienportella@gmail.com>
2019-03-31 15:16:53 +0200

    started

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
 if __name__ == '__main__':
+
+
+def mathingMap(img, template):
+    rows,cols = img.shape
+    trows,tcols = template.shape
+    matching_map = np.zeros(rows-trows+1,cols-tcols+1)
+
+    
