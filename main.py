aa40117d7d10c8366a212fea5d9a6947e22643e8
Farmak09 <farmak9@gmail.com>
2019-03-31 14:58:29 +0200

    input added

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
+import cv2
+import numpy as np
+
+def GetTheInputs():
+    inputImg = input("Input image: ")
+    targetImg = input("Target image: ")
+    threshold = input("Detection threshold: ")
+    return inputImg, targetImg, threshold
+
 if __name__ == '__main__':
+    inpt, tgt, thrshd = GetTheInputs()
+    print(inpt)
+    print(tgt)
+    print(thrshd)
+
+    grayInputImg = cv2.imread(inpt, cv2.IMREAD_GRAYSCALE)
+    colorInputImg = cv2.imread(inpt, cv2.IMREAD_ANYCOLOR)
+    grayTargetImg = cv2.imread(tgt, cv2.IMREAD_GRAYSCALE)
+    colorTargetImg = cv2.imread(tgt, cv2.IMREAD_ANYCOLOR)
+
+
+
+
+
+
